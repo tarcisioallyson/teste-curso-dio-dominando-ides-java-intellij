622857311a3ad0862a1dad70dff97dd85ca90424
Tarcisio <tarcisioallyson@gmail.com>
2022-02-23 10:07:22 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,13 +1,18 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
+
+import java.util.Enumeration;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O problema dos 3 copos",300);
+        System.out.println(livro1);
 
         /*int a = 5;
         int b = 3;
